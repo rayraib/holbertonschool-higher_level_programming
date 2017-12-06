b42d1b443ae9a9f32e5e5e44fec6eb00f3b5d2bf
rayraib <binitarai11@gmail.com>
2017-12-06 05:02:08 +0000

    replace initial letters of each string with Uppercase letter

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,10 +2,10 @@
 def fizzbuzz():
     for n in range(1, 101):
         if n % 15 == 0:
-            print('fizzubzz', end=" ")
+            print('FizzBuzz', end=" ")
         elif n % 3 == 0:
-            print('fizz', end=" ")
+            print('Fizz', end=" ")
         elif n % 5 == 0:
-            print('buzz', end=" ")
+            print('Buzz', end=" ")
         else:
             print('{:d}'.format(n), end=" ")
