d8cb4d3132f68badfdcd00f95ed794d0e2f16006
rayraib <binitarai11@gmail.com>
2017-12-07 04:34:47 +0000

    rearrange code to utilize only one print function

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-flag = 0
 z = 122
+case = 0
 for i in range(26):
-    if flag == 0:
-        print("{}".format(chr(z - i)), end="")
+    print("{}".format(chr((z - i) - case)), end="")
+    if case == 0:
+        case = 32
     else:
-        print("{}".format(chr((z - i) - 32)), end="")
-    if flag == 0:
-        flag = 1
-    else:
-        flag = 0
+        case = 0
