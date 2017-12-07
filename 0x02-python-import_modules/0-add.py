297f5a04cfe28cdc5f2d28c98bd256b2bbdbaf0f
rayraib <binitarai11@gmail.com>
2017-12-07 20:22:33 +0000

    import the add function from module add_0 instead of importing the entire module

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import add_0
-a = 1
-b = 2
-result = add_0.add(a, b)
+from add_0 import add
 if __name__ == "__main__":
-	print("{} + {} = {}".format(a, b, result))
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
