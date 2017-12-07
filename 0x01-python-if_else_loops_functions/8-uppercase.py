a00f312a93d33d3e65864bec8761358d4f256e97
rayraib <binitarai11@gmail.com>
2017-12-07 03:32:21 +0000

    change if condition

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     for i in (str):
         distance = ord(i) - ord('A')
-        if ord(i) >= ord('a') and ord(i) <= ord('z'):
+        if ord('a') <= ord(i) <= ord('z'):
             distance = ord(i) - ord('a')
         final = chr(ord('A') + distance)
         print('{}'.format(final), end="")
