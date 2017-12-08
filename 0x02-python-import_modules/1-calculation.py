371c8743f5e06593ed5845801cf8cb1b0a3c7efe
rayraib <binitarai11@gmail.com>
2017-12-08 03:48:05 +0000

    remove comments

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    '''If the program is being executed directly, import
-    functions from module then, call each function and print
-    the return values'''
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
