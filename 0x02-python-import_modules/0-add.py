0de01bdddabb89cb77aecf659356156aeff79e5f
rayraib <binitarai11@gmail.com>
2017-12-08 03:33:39 +0000

    remove multiline comment

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 from add_0 import add
-'''If the program is being directly executed and not imported as module,
-then, import function add from module add_0 and call add on the two
-variables a and b'''
 if __name__ == "__main__":
     a = 1
     b = 2
