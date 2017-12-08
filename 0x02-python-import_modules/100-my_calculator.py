42a1d387c92535d8c5266c1dbeb76c1b027c93f9
rayraib <binitarai11@gmail.com>
2017-12-08 02:13:36 +0000

    add comments and typecast the input arguments at index 1 and 3 as integers

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,23 +1,32 @@
 #!/usr/bin/python3
+'''Check whether the file is being executed directly or imported
+   as module in another file and only if executed directly continue
+   with the rest of the code'''
 if __name__ == "__main__":
     import sys
-    import calculator_1
-    arg = sys.argv
-    arg_cnt = len(sys.argv)
-    if arg_cnt != 4:
-        print("Usage: {} <a> <operator> <b>".format(arg[0]))
+    from calculator_1 import add, sub, mul, div
+    argc = len(sys.argv)
+    '''Check if there are exactly 4 arguments: filename, number, operand
+    and second number'''
+    if argc != 4:
+        print("Usage: {} <a> <operator> <b>".format(sys.argv[0]))
         exit(1)
-    if arg[2] == '+':
-        print("{} + {} = {}".format(arg[1], arg[3], calculator_1.add(arg[1], arg[3])))
-    elif arg[2] == '-':
-        print("{} - {} = {}".format(arg[1], arg[3],
-                                    calculator_1.sub(arg[1], arg[3])))
-    elif arg[2] == '*':
-        print("{} * {} = {}".format(arg[1], arg[3],
-                                    calculator_1.mul(arg[1], arg[3])))
-    elif arg[2] == '/':
-        print("{} / {} = {}".format(arg[1], arg[3],
-                                    calculator_1.div(arg[1], arg[3])))
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    op = sys.argv[2]
+    '''Check if the operand is one of the four options and if yes,
+    call the respective imported function and print the returned
+    result'''
+    if op == '+':
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif op == '-':
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif op == '*':
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    elif op == '/':
+        print("{} / {} = {}".format(a, b, div(a, b)))
+        '''If the input operand is not one of the four valid options
+        print an error mesasge'''
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
