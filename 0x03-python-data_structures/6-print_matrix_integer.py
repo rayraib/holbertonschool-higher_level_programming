d2d96dd425771bd32c8d5e5182ba3a91df58880d
rayraib <binitarai11@gmail.com>
2017-12-12 20:11:03 +0000

    initial commit

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for column in row:
+            if column != row[len(row) - 1]:
+                print ("{} ".format(column), end="")
+            else:
+                print ("{}".format(column))
