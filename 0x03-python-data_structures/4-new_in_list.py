5d6bb64b65a5927ec978ec136cf41b2a6107dccb
rayraib <binitarai11@gmail.com>
2017-12-12 23:27:22 +0000

    add an else statement to prevent double addition at the 'idx' index

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,8 +3,9 @@ def new_in_list(my_list, idx, element):
     new_list = []
     if idx < 0 or idx >= len(my_list):
         return my_list
-    for i in range(len(my_list) - 1):
+    for i in range(len(my_list)):
         if i == idx:
             new_list.append(element)
-        new_list.append(my_list[i])
+        else:
+            new_list.append(my_list[i])
     return new_list
