6bc3db5f7c6174ff94942d545b5b9761f410552e
rayraib <binitarai11@gmail.com>
2017-12-12 20:18:23 +0000

    add :d format specifier for integer values

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for column in row:
             if column != row[len(row) - 1]:
-                print ("{} ".format(column), end="")
+                print ("{:d} ".format(column), end="")
             else:
-                print ("{}".format(column))
+                print ("{:d}".format(column))
