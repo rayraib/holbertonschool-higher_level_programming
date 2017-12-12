e273244f0dad0993c3dd68f6d66dc09ebfa0cc84
rayraib <binitarai11@gmail.com>
2017-12-12 20:16:20 +0000

    add :d format specifier for integers

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,4 +3,4 @@ def print_reversed_list_integer(my_list=[]):
     for i in range(len(my_list) + 1):
         if i == 0:
             continue
-        print("{}".format(my_list[-i]))
+        print("{:d}".format(my_list[-i]))
