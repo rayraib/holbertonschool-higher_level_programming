fd73805b5db92b7ec537a4115ae45047b9360f24
rayraib <binitarai11@gmail.com>
2017-12-13 02:04:06 +0000

    reinstate the if condition

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for column in row:
-            print("{:d} ".format(column), end="")
+            if column != row[len(row) - 1]:
+                print("{:d} ".format(column), end="")
+            else:
+                print("{}".format(column), end="")
         print("\n", end="")
