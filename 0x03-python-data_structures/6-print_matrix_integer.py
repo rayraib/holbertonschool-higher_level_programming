be1004bf67df925899dc49e149ec1d31e7660803
rayraib <binitarai11@gmail.com>
2017-12-13 01:58:31 +0000

    add space after each number while printing to standard output

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for column in row:
-            print("{:d}".format(column), end="")
-        print("\n", end="")
+    if len(matrix) > 0:
+        for row in matrix:
+            for column in row:
+                print("{:d} ".format(column), end="")
+            print("\n", end="")
