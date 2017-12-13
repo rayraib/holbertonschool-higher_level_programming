61f460eff3733e26741511120bdff7382c8f3ea1
rayraib <binitarai11@gmail.com>
2017-12-13 01:55:38 +0000

    remove if statement condition and insert a print with newline after the inner for loop

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for column in row:
-            if column != row[len(row) - 1]:
-                print ("{:d} ".format(column), end="")
-            else:
-                print ("{:d}".format(column))
+            print("{:d}".format(column), end="")
+        print("\n", end="")
