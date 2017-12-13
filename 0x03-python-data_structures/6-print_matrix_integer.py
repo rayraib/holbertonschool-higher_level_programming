040f67c98ea8e554752ac0b5b96451061379aada
rayraib <binitarai11@gmail.com>
2017-12-13 01:59:50 +0000

    remove if statement

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if len(matrix) > 0:
-        for row in matrix:
-            for column in row:
-                print("{:d} ".format(column), end="")
-            print("\n", end="")
+    for row in matrix:
+        for column in row:
+            print("{:d} ".format(column), end="")
+        print("\n", end="")
