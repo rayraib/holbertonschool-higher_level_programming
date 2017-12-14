e10a70dda45fd5fd4cb948a28add5cbe52a67eb7
rayraib <binitarai11@gmail.com>
2017-12-14 22:03:10 +0000

    remove for loop and use if statement to check for key in my_dict

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def update_dictionary(my_dict, key, value):
-    for k in my_dict.items():
-        if k == key:
-            my_dict[k] = value
+    if key in my_dict:
+        my_dict[key] = value
     my_dict[key] = value
     return my_dict
