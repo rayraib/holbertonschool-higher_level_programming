f622a51bbc31a5b92bf3de4324103f9883a85b8a
rayraib <binitarai11@gmail.com>
2017-12-14 23:21:13 +0000

    remove assignment of the number list and create the list inside the map function

--- 0x04-python-more_data_structures/11-mutiply_list_map.py
+++ 0x04-python-more_data_structures/11-mutiply_list_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def mutiply_list_map(my_list=[], number=0):
-    num_list = [number] * len(my_list)
-    return (list(map(lambda val, num: val * num, my_list, num_list)))
+    return (list(map(lambda x, y: x * y, my_list, [number] * len(my_list))))
