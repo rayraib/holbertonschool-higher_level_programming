8d3bd9d3a46d47bc3b11844c8dfb69595a4fb98f
rayraib <binitarai11@gmail.com>
2017-12-15 19:18:16 +0000

    instead of inputing the list of kyes of function max, give my_dict as the argument so that it returns both key and value and not just the max value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def best_score(my_dict):
     if my_dict:
-        value_list = list(my_dict.values())
-        return (max(value_list))
+        return (max(my_dict))
     return None
