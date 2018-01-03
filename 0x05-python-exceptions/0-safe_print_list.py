43428f200f8e5214539955af10c0ba09bf879545
rayraib <binitarai11@gmail.com>
2018-01-03 00:24:03 +0000

    remove whitespace for pep8 compliant

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    i = 0
-    while (i < x):
+    for i in range(x):
         try:
             print ("{}".format(my_list[i]), end="")
             i = i + 1
