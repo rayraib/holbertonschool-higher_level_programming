af8a1d113dd15ead91013906af3c6680220a110e
rayraib <binitarai11@gmail.com>
2018-01-03 23:17:31 +0000

    remove getter and setter methods

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,15 +3,6 @@ class Square():
     """Represent a square with size"""
     def __init__(self, size):
         """Initializes the data"""
-        self.size = size 
-
-    @property
-    def size(self):
-        """Return the size value"""
-        return self._size
-    @size.setter
-    def size(self, size):
-        """Set the private __size attribute to size"""
         self.__size = size
 
      
