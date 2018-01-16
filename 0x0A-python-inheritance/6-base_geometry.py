fc934d83ab634c117de38e406a8e362aab07708d
rayraib <binitarai11@gmail.com>
2018-01-16 21:03:04 +0000

    initial commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''BaseGeometry'''
+
+
+class BaseGeometry:
+    '''BaseGeometry'''
+    def area(self):
+        ''' calculate area of base geometry'''
+        raise Exception("area() is not implemented")
