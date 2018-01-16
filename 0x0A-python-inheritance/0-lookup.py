1da31a3f58043f55a32be90b5ffe551526084c79
rayraib <binitarai11@gmail.com>
2018-01-16 18:02:38 +0000

    initial commit

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''Return available attributes and method of an object'''
+
+
+def lookup(obj):
+    '''return available attributes and methods of an object'''
+    return (dir(obj))
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,3 @@
+# 0x0A-python-inheritance
+### Synopsis: Project to learn and understand theory and implemenation of inheritence in Python.
+
