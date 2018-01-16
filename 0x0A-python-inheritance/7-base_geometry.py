3996bc3b804d29004caa40e0417c70d8fdfee9f4
rayraib <binitarai11@gmail.com>
2018-01-16 21:58:27 +0000

    initial commit

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''Base class for geometry shapes'''
+
+
+class BaseGeometry:
+    '''base class geometry'''
+    def area(self):
+        ''' area of  geometric shape'''
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        '''validate if the value is an int'''
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
