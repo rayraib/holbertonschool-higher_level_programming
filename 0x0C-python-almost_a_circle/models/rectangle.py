dd2368d862d573055d599630ed71f08d1964c024
rayraib <binitarai11@gmail.com>
2018-01-19 21:10:07 +0000

    assing id as arg for super call

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -11,8 +11,7 @@ class Rectangle(Base):
         self.__height = height
         self.__x = x
         self.__y = y
-        self.id = id
-        super().__init__(self.id)
+        super().__init__(id)
 
     @property
     def width(self):
