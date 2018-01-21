32e5a9f185c15c9bba3ef43ea46da1cfecd7ae17
rayraib <binitarai11@gmail.com>
2018-01-21 17:54:31 +0000

    add to_json_string method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+import json
 '''
     Base class for geomtric shapes
 '''
 
 
-class Base:
+class Base(object):
     '''
         Defines a base class that other geometric shapes can inherit from
     '''
@@ -17,3 +18,9 @@ class Base:
             self.id = Base.__nb_objects
         else:
             self.id = id
+
+    def to_json_string(list_dictionaries):
+        '''convert list_dictionaries to json string and return'''
+        if list_dictionaries is None:
+            return ("[]")
+        return (json.dumps(list_dictionaries))
