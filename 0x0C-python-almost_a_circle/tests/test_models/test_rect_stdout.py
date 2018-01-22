acdc9a05484a29f45bafaf41b24002197c9b2604
rayraib <binitarai11@gmail.com>
2018-01-22 18:02:54 +0000

    change tests to caputre result of calling str

--- 0x0C-python-almost_a_circle/tests/test_models/test_rect_stdout.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rect_stdout.py
@@ -21,20 +21,10 @@ class test_rect_stdout(unittest.TestCase):
         '''
             test the __str__ method of the Rectangle class
         '''
-        with patch('sys.stdout', new=StringIO()) as fakeOutPut:
-            print(Rectangle(4, 6, 2, 1, 12))
-            self.assertEqual(fakeOutPut.getvalue(),
-                             "[Rectangle] (12) 2/1 - 4/6\n")
-        #ID is dynamic
-        with patch('sys.stdout', new=StringIO()) as fakeOutPut:
-            print(Rectangle(5, 5, 1))
-            self.assertEqual(fakeOutPut.getvalue(),
-                             "[Rectangle] (1) 1/0 - 5/5\n")
-        #requires ID value to be adjusted to be dynamic
-        with patch('sys.stdout', new=StringIO()) as fakeOutPut:
-            print(Rectangle(1, 1))
-            self.assertEqual(fakeOutPut.getvalue(),
-                             "[Rectangle] (1) 0/0 - 1/1\n")
+        result = str(Rectangle(4, 6, 2, 1, 12))
+        self.assertEqual(result, "[Rectangle] (12) 2/1 - 4/6")
+        result = str(Rectangle(5, 5, 1, 2, 3))
+        self.assertEqual(result, "[Rectangle] (3) 1/2 - 5/5")
 
     def test_display_method(self):
         '''
@@ -69,48 +59,21 @@ class test_rect_stdout(unittest.TestCase):
             Each attribute in the *arg is in order of
             ID, Width, Height, X and Y
         '''
-        with patch('sys.stdout', new=StringIO()) as fakeOutPut:
-            r = Rectangle(10, 10, 10, 10, 10)
-            print(r)
-            self.assertEqual(fakeOutPut.getvalue(),
-                             "[Rectangle] (10) 10/10 - 10/10\n")
-
-        with patch('sys.stdout', new=StringIO()) as fakeOutPut:
-            r.update(89) 
-            print(r)
-            self.assertEqual(fakeOutPut.getvalue(),
-                             "[Rectangle] (89) 10/10 - 10/10\n")
-
-        with patch('sys.stdout', new=StringIO()) as fakeOutPut:
-            r.update(8, 9) 
-            print(r)
-            self.assertEqual(fakeOutPut.getvalue(),
-                             "[Rectangle] (8) 10/10 - 9/10\n")
-
-        with patch('sys.stdout', new=StringIO()) as fakeOutPut:
-            r.update(8, 9, 11) 
-            print(r)
-            self.assertEqual(fakeOutPut.getvalue(),
-                             "[Rectangle] (8) 10/10 - 9/11\n")
-
-        with patch('sys.stdout', new=StringIO()) as fakeOutPut:
-            r.update(9, 10, 13, 12) 
-            print(r)
-            self.assertEqual(fakeOutPut.getvalue(),
-                             "[Rectangle] (9) 12/10 - 10/13\n")
-
-        with patch('sys.stdout', new=StringIO()) as fakeOutPut:
-            r.update(9, 10, 13, 12, 14) 
-            print(r)
-            self.assertEqual(fakeOutPut.getvalue(),
-                             "[Rectangle] (9) 12/14 - 10/13\n")
-
-        with patch('sys.stdout', new=StringIO()) as fakeOutPut:
-            ls = [2, 3, 4]
-            r.update(*ls) 
-            print(r)
-            self.assertEqual(fakeOutPut.getvalue(),
-                             "[Rectangle] (2) 12/14 - 3/4\n")
+        r = Rectangle(10, 10, 10, 10, 10)
+        self.assertEqual(str(r), "[Rectangle] (10) 10/10 - 10/10")
+        r.update(89) 
+        self.assertEqual(str(r), "[Rectangle] (89) 10/10 - 10/10")
+        r.update(8, 9) 
+        self.assertEqual(str(r), "[Rectangle] (8) 10/10 - 9/10")
+        r.update(8, 9, 11) 
+        self.assertEqual(str(r), "[Rectangle] (8) 10/10 - 9/11")
+        r.update(9, 10, 13, 12) 
+        self.assertEqual(str(r), "[Rectangle] (9) 12/10 - 10/13")
+        r.update(9, 10, 13, 12, 14) 
+        self.assertEqual(str(r), "[Rectangle] (9) 12/14 - 10/13")
+        ls = [2, 3, 4]
+        r.update(*ls) 
+        self.assertEqual(str(r), "[Rectangle] (2) 12/14 - 3/4")
 
     def test_update_kwargs(self):
         '''
@@ -119,35 +82,17 @@ class test_rect_stdout(unittest.TestCase):
             a dictionary
         '''
 
-        with patch('sys.stdout', new=StringIO()) as fakeOutPut:
-            r = Rectangle(10, 10, 10, 10, 10)
-            print(r)
-            self.assertEqual(fakeOutPut.getvalue(),
-                             "[Rectangle] (10) 10/10 - 10/10\n")
-
-        with patch('sys.stdout', new=StringIO()) as fakeOutPut:
-            r.update(height=1)
-            print(r)
-            self.assertEqual(fakeOutPut.getvalue(),
-                             "[Rectangle] (10) 10/10 - 10/1\n")
-
-        with patch('sys.stdout', new=StringIO()) as fakeOutPut:
-            r.update(width=1, x=2)
-            print(r)
-            self.assertEqual(fakeOutPut.getvalue(),
-                             "[Rectangle] (10) 2/10 - 1/1\n")
-
-        with patch('sys.stdout', new=StringIO()) as fakeOutPut:
-            r.update(y=1, width=2, x=3, id=89)
-            print(r)
-            self.assertEqual(fakeOutPut.getvalue(),
-                             "[Rectangle] (89) 3/1 - 2/1\n")
-
-        with patch('sys.stdout', new=StringIO()) as fakeOutPut:
-            r.update(x=1, height=2, y=3, width=4)
-            print(r)
-            self.assertEqual(fakeOutPut.getvalue(),
-                             "[Rectangle] (89) 1/3 - 4/2\n")
+        r = Rectangle(10, 10, 10, 10, 10)
+        print(r)
+        self.assertEqual(str(r), "[Rectangle] (10) 10/10 - 10/10")
+        r.update(height=1)
+        self.assertEqual(str(r), "[Rectangle] (10) 10/10 - 10/1")
+        r.update(width=1, x=2)
+        self.assertEqual(str(r), "[Rectangle] (10) 2/10 - 1/1")
+        r.update(y=1, width=2, x=3, id=89)
+        self.assertEqual(str(r), "[Rectangle] (89) 3/1 - 2/1")
+        r.update(x=1, height=2, y=3, width=4)
+        self.assertEqual(str(r), "[Rectangle] (89) 1/3 - 4/2")
 
     def test_update_arg_kwargs(self):
         '''
@@ -155,23 +100,9 @@ class test_rect_stdout(unittest.TestCase):
             and **kwargs input
             if *arg exists and not empty, skip **kwarg
         '''
-        with patch('sys.stdout', new=StringIO()) as fakeOutPut:
-            r = Rectangle(10, 10, 10, 10, 10)
-            print(r)
-            self.assertEqual(fakeOutPut.getvalue(),
-                             "[Rectangle] (10) 10/10 - 10/10\n")
-
-        with patch('sys.stdout', new=StringIO()) as fakeOutPut:
-            r.update(1, 2, x=9)
-            print(r)
-            self.assertEqual(fakeOutPut.getvalue(),
-                             "[Rectangle] (1) 10/10 - 2/10\n")
-
-        with patch('sys.stdout', new=StringIO()) as fakeOutPut:
-            r.update(9, x=9)
-            print(r)
-            self.assertEqual(fakeOutPut.getvalue(),
-                             "[Rectangle] (9) 10/10 - 2/10\n")
-
-            #        with self.assertRaisesRegexp(SyntaxError, "non-keyword arg after keyword arg"):
-            #r.update(height=9, 8)
+        r = Rectangle(10, 10, 10, 10, 10)
+        self.assertEqual(str(r), "[Rectangle] (10) 10/10 - 10/10")
+        r.update(1, 2, x=9)
+        self.assertEqual(str(r), "[Rectangle] (1) 10/10 - 2/10")
+        r.update(9, x=9)
+        self.assertEqual(str(r), "[Rectangle] (9) 10/10 - 2/10")
