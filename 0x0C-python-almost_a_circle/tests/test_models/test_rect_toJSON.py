61d2c12c7a87a9630b17c724ed663bca7c7288dd
rayraib <binitarai11@gmail.com>
2018-01-22 21:42:59 +0000

    add to_JSON method test

--- 0x0C-python-almost_a_circle/tests/test_models/test_rect_toJSON.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rect_toJSON.py
@@ -15,13 +15,37 @@ class test_class_method(unittest.TestCase):
     '''
     def test_tojson_method(self):
         '''
-            Test the to_json metho of the Rectangle's superclass
+            Test the to_json method of the Rectangle's superclass
         '''
         r1 = Rectangle(10, 7, 2, 8, 9)
         dictionary = r1.to_dictionary()
         json_dictionary = r1.to_json_string([dictionary])
-        self.assertEqual(dictionary, {'x': 2, 'width': 10, 'id': 9, 'height': 7, 'y': 8})
         self.assertEqual(type(dictionary), dict)
-        self.assertEqual(json_dictionary, '[{"x": 2, "width": 10, "id": 9, "height": 7, "y": 8}]')
+        d = r1.from_json_string(json_dictionary)
+        self.assertEqual(d, [dictionary])
         self.assertEqual(type(json_dictionary), str)
 
+    def test_savetofile_method(self):
+        '''
+            test the class method save_to_file:
+            write JSON string to a file
+        '''
+        r1 = Rectangle(10, 7, 2, 8, 9)
+        Rectangle.save_to_file([r1])
+        with open("Rectangle.json", "r") as file:
+            result = file.read()
+            result = Rectangle.from_json_string(result)
+            self.assertEqual(result, [{"y": 8, "x": 2, "id": 9, "width": 10, "height": 7}])
+        r2 = Rectangle(2, 4)
+        r2.update(id=99)
+        Rectangle.save_to_file([r2])
+        with open("Rectangle.json", "r") as file:
+            result = file.read()
+            result = Rectangle.from_json_string(result)
+            self.assertEqual(result, [{"y": 0, "x": 0, "id": 99, "width": 2, "height": 4}])
+
+    def test_from_json_to_string(self):
+        '''
+            test the class method that returns a list of json string representation
+        '''
+        pass
