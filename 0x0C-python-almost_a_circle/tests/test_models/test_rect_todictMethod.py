ee35997dd2ad4654aaa33174bfd110b3c7a07318
rayraib <binitarai11@gmail.com>
2018-01-23 17:31:44 +0000

    betty and pep8 compliant

--- 0x0C-python-almost_a_circle/tests/test_models/test_rect_todictMethod.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rect_todictMethod.py
@@ -17,11 +17,12 @@ class TestRectangleclass(unittest.TestCase):
     def test_to_dict(self):
         '''
             test the to_dict method of a rectangle
-            should return the object as a dictionary 
+            should return the object as a dictionary
         '''
         r1 = Rectangle(10, 2, 1, 9, 8)
         r1_dictionary = r1.to_dictionary()
-        self.assertEqual(r1_dictionary, {'x': 1, 'y': 9, 'id': 8, 'height': 2, 'width': 10})
+        self.assertEqual(r1_dictionary, {'x': 1, 'y': 9,
+                         'id': 8, 'height': 2, 'width': 10})
         self.assertEqual(type(r1_dictionary), dict)
         r2 = Rectangle(1, 1)
         r2.update(**r1_dictionary)
