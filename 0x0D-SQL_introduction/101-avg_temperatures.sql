9675b146328d31295766da52cca2f5c7866be57f
rayraib <binitarai11@gmail.com>
2018-02-27 04:03:34 +0000

    change tmp to temp

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,5 @@
 -- Display the average temperatures by city ordered by temperatures
-SELECT city, AVG(value) AS avg_tmp
+SELECT city, AVG(value) AS avg_temp
 FROM temperatures
 GROUP BY city
-ORDER BY avg_tmp DESC;
+ORDER BY avg_temp DESC;
