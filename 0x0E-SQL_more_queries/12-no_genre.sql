1b555a1c292ab81601ae23c09caeb9ebd17af3cf
rayraib <binitarai11@gmail.com>
2018-03-01 23:34:10 +0000

    show NULLs

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,5 @@
 -- List all shows contained in the hbtn_0d_tvshows without a genre linked
 SELECT title, genre_id
 FROM tv_shows LEFT OUTER JOIN tv_show_genres
-ON show_id IS NULL 
+ON genre_id IS NULL 
 ORDER BY title ASC, genre_id ASC;
