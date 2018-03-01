c57d0136c39c3f15eb666fc950f40f2e50795735
rayraib <binitarai11@gmail.com>
2018-03-01 23:39:57 +0000

    merge before

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,6 @@
 -- List all shows contained in the hbtn_0d_tvshows without a genre linked
 SELECT title, genre_id
 FROM tv_shows LEFT OUTER JOIN tv_show_genres
-ON genre_id IS NULL 
+ON show_id = tv_shows.id 
+WHERE genre_id IS NULL 
 ORDER BY title ASC, genre_id ASC;
