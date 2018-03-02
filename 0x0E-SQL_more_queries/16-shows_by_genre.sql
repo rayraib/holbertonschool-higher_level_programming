e629f86c1ae3eb513cdb47b0c9ebf82c10c1d53a
rayraib <binitarai11@gmail.com>
2018-03-02 00:38:26 +0000

    list all shows and genres linked to that show

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- Lists all shows and all genres linked
+SELECT tv_shows.title, tv_genres.name
+FROM tv_show_genres RIGHT OUTER JOIN tv_genres
+ON tv_genres.id = tv_show_genres.genre_id
+RIGHT OUTER JOIN tv_shows
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY title ASC, name ASC;
