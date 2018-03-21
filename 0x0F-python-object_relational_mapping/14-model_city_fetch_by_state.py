8ac78ed9c958c5e01f04d5e30cec588d7e45ecbe
rayraib <binitarai11@gmail.com>
2018-03-21 19:14:13 +0000

    return all the cities with foreign key to states

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''
+    Script that prints all City objects from the database hbtn_0e_14_usa:
+'''
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+from model_state import State, Base
+from model_city import City
+import sys
+
+
+if __name__ == "__main__":
+    uname = sys.argv[1]
+    pw = sys.argv[2]
+    db = sys.argv[3]
+    #Create engine that opens connection between the class state and
+    # the database with the data
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                          (uname, pw, db), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    #create session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    #Delete instance with 'a' in their name
+    for c_instance, s_instance in session.query(City, State).filter(
+            City.state_id == State.id).order_by(City.id).all():
+        print("{}: ({}) {}".format(s_instance.name,
+                                   c_instance.id, c_instance.name))
