1ea8713fe9b825c84014eda459b5eed48f35f274
rayraib <binitarai11@gmail.com>
2018-03-21 16:09:38 +0000

    select State instance with 'a'

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''
+    Script that lists all State objects that contain the letter a
+    from the database hbtn_0e_6_usa
+'''
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+from model_state import State, Base
+import sys
+
+
+if __name__ == "__main__":
+    uname = sys.argv[1]
+    pw = sys.argv[2]
+    db = sys.argv[3]
+
+    #Create engine that opens connection between the class state and
+    # the database with the data
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                          (uname, pw, db), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    #create session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    #query the database
+    for instance in session.query(State).\
+            filter(State.name.ilike('%a%')).\
+            order_by(State.id):
+        print("{}: {}".format(instance.id, instance.name))
