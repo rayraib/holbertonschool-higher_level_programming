be724ffe1788beb4f082e02f2309923341f81521
rayraib <binitarai11@gmail.com>
2018-03-21 17:14:08 +0000

    change state's name with id of 2 to New Mexico

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''
+    script that adds the State object “Louisiana” to the database hbtn_0e_6_usa
+'''
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+from model_state import State, Base
+from sqlalchemy import update
+import sys
+
+
+if __name__ == "__main__":
+    uname = sys.argv[1]
+    pw = sys.argv[2]
+    db = sys.argv[3]
+    #Create engine that opens connection between the class state and
+    # the database with the data
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                          (uname, pw, db), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    #create session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    #update State where id = 2 to New Mexico
+    for instance in session.query(State).filter(State.id == 2):
+        instance.name = "New Mexico"
+        session.commit()
