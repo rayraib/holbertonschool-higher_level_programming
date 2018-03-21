3946ee8281383c9fb0456052ebce9196c52e6d83
rayraib <binitarai11@gmail.com>
2018-03-21 20:51:01 +0000

    use format to create the query

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,15 +9,17 @@ import sys
 if __name__ == "__main__":
     db = MySQLdb.connect(
         host='localhost',
+        port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3]
         )
     state = sys.argv[4]
+    query = ("SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC"
+             .format(state))
     cs = db.cursor()
-    cs.execute('USE hbtn_0e_0_usa')
-    cs.execute("SELECT * FROM states WHERE name = %s ORDER BY states.id ASC",
-              (state,))
+    cs.execute(query)
     rows = cs.fetchall()
     for row in rows:
         print(row)
+    db.close()
