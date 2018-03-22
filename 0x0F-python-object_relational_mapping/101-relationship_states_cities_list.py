05fbf4cfc786789101234a4a2fac6711c41fe7c7
rayraib <binitarai11@gmail.com>
2018-03-22 04:06:08 +0000

    query state and city through state only

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''
+    script that lists all State objects, and corresponding City objects,
+    contained in the database hbtn_0e_101_usa
+'''
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+from relationship_city import City, Base
+from relationship_state import State
+import sys
+
+
+if __name__ == "__main__":
+    uname = sys.argv[1]
+    pw = sys.argv[2]
+    db = sys.argv[3]
+    # Create engine that opens connection between the class state and
+    # the database with the data
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (uname, pw, db), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    # create session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    #
+    for state in session.query(State).all():
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
+            print("    {}: {}".format(city.id, city.name))
+    session.close()
