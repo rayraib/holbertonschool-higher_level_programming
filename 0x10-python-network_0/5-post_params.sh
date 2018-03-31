0cf9e8f4935be87db143799be865b358b16f0a70
rayraib <binitarai11@gmail.com>
2018-03-31 16:50:40 +0000

    initial commit

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# Bash script that takes in a URL, sends a POST request to the passed URL,
+# and displays the body of the response
+curl -s "$1" -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
