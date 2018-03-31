0b8cef1e7afba0d38c89bca7aa4b0b32e57a5a5e
rayraib <binitarai11@gmail.com>
2018-03-31 20:44:41 +0000

    fix variable names

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -9,14 +9,13 @@ def find_peak(list_of_integers):
         find the peak
     '''
     length = len(list_of_integers)
-    if length < 3:
-        return
+    for i in range(1, length - 1):
+        if (list_of_integers[i - 1] < list_of_integers[i]
+                > list_of_integers[i + 1]):
+            return(list_of_integers[i])
+    if list_of_integers[0] > list_of_integers[1]:
+        return(list_of_integers[0])
+    elif list_of_integers[length - 1] > list_of_integers[length - 2]:
+        return(list_of_integers[length - 1])
     else:
-        for i in range(1, length - 1):
-            if (list_of_integers[i - 1] < list_of_integers[i]
-                    > list_of_integers[i + 1]):
-                return(list_of_integers[i])
-        if list_of_integers[0] > list_of_integer[1]:
-            return(list_of_integers[0])
-        elif list_of_integers[length - 1] > list_of_integer[length - 2]:
-            return(list_of_integers[length - 1])
+        return list_of_integers[0]
