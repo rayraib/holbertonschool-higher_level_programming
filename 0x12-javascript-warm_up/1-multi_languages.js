91f46e73515688c4bd31adc667d42ee73fff67e6
rayraib <binitarai11@gmail.com>
2018-04-29 11:24:54 +0000

    use let instead of var

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -2,6 +2,6 @@
 
 let quotes = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 let len = quotes.length;
-for (var i = 0; i < len; i++) {
+for (let i = 0; i < len; i++) {
   console.log(quotes[i]);
 }
