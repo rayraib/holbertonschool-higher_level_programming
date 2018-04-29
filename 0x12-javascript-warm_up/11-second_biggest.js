eae8e5829789627b5bba71711b29af3e879c24d3
rayraib <binitarai11@gmail.com>
2018-04-29 12:45:31 +0000

    update initial value

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,13 +2,15 @@
 
 let arg = process.argv;
 let len = arg.length;
-let big = 0;
-let pBig = big;
+let big;
+let pBig;
 let i;
 
 if (len <= 3) {
   console.log('0');
 } else {
+  big = parseInt(arg[2]);
+  pBig = big;
   for (i = 2; i < len; i++) {
     if (big < parseInt(arg[i])) {
       pBig = big;
