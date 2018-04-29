77138b067cb19991fad5384046c61045e1247080
rayraib <binitarai11@gmail.com>
2018-04-29 11:23:53 +0000

    check if there is more than one argument

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,6 +4,8 @@ let len = process.argv.length;
 
 if (len <= 2) {
   console.log('No argument');
-} else {
+} else if (len === 3) {
   console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
