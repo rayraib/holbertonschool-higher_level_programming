e1ca2ec1165014aa57eebce63c99b086989015cf
rayraib <binitarai11@gmail.com>
2018-04-29 12:28:06 +0000

    typecast to string

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,15 +3,16 @@
 let arg = process.argv;
 let len = arg.length;
 let big = 0;
-let pBig = big, i;
+let pBig = big;
+let i;
 
 if (len <= 3) {
   console.log('0');
 } else {
   for (i = 2; i < len; i++) {
-    if (big < arg[i]) {
+    if (big < parseInt(arg[i])) {
       pBig = big;
-      big = arg[i];
+      big = parseInt(arg[i]);
     }
   }
   console.log(pBig);
