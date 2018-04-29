905be51037e31effb3e99159e4ead0fc5ca1cc67
rayraib <binitarai11@gmail.com>
2018-04-29 12:06:49 +0000

    factorial function

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+let arg = process.argv;
+let n = parseInt(arg[2]);
+
+function fact (a) {
+  if (isNaN(a) || a <= 1) {
+    return 1;
+  } else {
+    return (a + fact(a - 1));
+  }
+}
+let result = fact (n);
+console.log(result);
