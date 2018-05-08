426d3747c603ad78d6c711802012c8a8f13d5210
rayraib <binitarai11@gmail.com>
2018-05-08 19:54:43 +0000

    convert data to string

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,7 +4,7 @@ let file = process.argv[2];
 
 let fs = require('fs');
 
-fs.readFile(file, 'utf-8', function (err, data) {
+fs.readFile(file, function (err, data) {
   if (err) throw err;
   console.log(data.toString());
 });
