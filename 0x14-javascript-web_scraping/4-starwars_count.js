2196cf91f76a9ee45e04084b2773f25de8579f9a
rayraib <binitarai11@gmail.com>
2018-05-08 19:45:52 +0000

    use the given arg for url

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,11 +1,23 @@
 #!/usr/bin/node
 
-let url = 'http://swapi.co/api/people/18';
+let url = process.argv[2];
 let request = require('request');
+let compare = 'https://swapi.co/api/people/18/';
+let count = 0;
 
 request(url, function (error, response, body) {
   if (error) throw error;
   let content = JSON.parse(body);
-  let movies = content['films'].length;
-  console.log(movies);
+  let results = content['results'];
+  //  console.log(results[1])
+  for (let i = 0; i < results.length; i++) {
+    let content = results[i];
+    let characters = content['characters'];
+    for (let i = 0; i < characters.length; i++) {
+      if (characters[i] === compare) {
+        count++;
+      }
+    }
+  }
+  console.log(count);
 });
