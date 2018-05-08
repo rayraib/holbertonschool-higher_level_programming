63b03c2b4661cd0c5f5c482c778da82dcec5720b
rayraib <binitarai11@gmail.com>
2018-05-08 19:57:38 +0000

    use console.log print error

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,7 +4,10 @@ let file = process.argv[2];
 
 let fs = require('fs');
 
-fs.readFile(file, function (err, data) {
-  if (err) throw err;
-  console.log(data.toString());
+fs.readFile(file, 'utf-8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
